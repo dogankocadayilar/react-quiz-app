82e2b9e71a6da5dec501b233b6f676374c58d7a2
dogankocadayilar <dogankocadayilar@gmail.com>
2023-01-19 12:43:21 +0300

    Update OpenScreen

--- src/components/OpenScreen.jsx
+++ src/components/OpenScreen.jsx
@@ -2,7 +2,7 @@ function OpenScreen(props) {
   return (
     <main>
       <h1 className="start-title">Quizzical</h1>
-      <p className="start-desc">Description</p>
+      <p className="start-desc">Test your knowledge!</p>
       <button className="start-button" onClick={() => props.setStartQuiz(true)}>
         Start quiz
       </button>
